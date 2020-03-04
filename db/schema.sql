860941008557f6ec0208e172717af976dc0ffe2c
HanFue282 <59034654+HanFue282@users.noreply.github.com>
2020-03-04 10:50:42 -0600

    minor addjustment

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,6 @@ CREATE TABLE pokemon
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
 	type varchar(30) NOT NULL,
-	natureID INTEGER(10) NULL,
 	-- stats start below
     hp INTEGER(3) NOT NULL,
     attack INTEGER(3) NOT NULL, 
@@ -24,7 +23,6 @@ CREATE TABLE generated_pokemon
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
 	type varchar(30) NOT NULL,
-	natureID INTEGER(10) NULL,
     hp INTEGER(3) NOT NULL,
     attack INTEGER(3) NOT NULL, 
     defense INTEGER(3) NOT NULL,
