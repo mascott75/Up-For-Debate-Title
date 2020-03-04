f771d092224beeb35a024b9d5ecd9e625d0ffe31
HanFue282 <59034654+HanFue282@users.noreply.github.com>
2020-03-04 12:27:57 -0600

    adjusted controller and pokemonModel.js

--- controller/pokemonController.js
+++ controller/pokemonController.js
@@ -7,49 +7,60 @@ var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
 var pokemon = require("../models/pokemonModel.js");
+var generate = require("../models/generated_pokemon");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    pokemon.all(function () {
-        res.render("?");
+    pokemon.all(function() {
+        var pokemon = {
+            pokemons: data
+        }
+        console.log(pokemon);
+        res.render("?", pokemon);
+    });
+});
+
+router.get("/generate", function (req, res) {
+    generate.all(function() {
+        var generate = {
+            generated = data
+        }
+        console.log(generate);
+        res.render("?", generate);
     });
 });
 
 router.post("/api/generate", function (req, res) {
-    burger.create([
-        "name", "eaten"
+    generate.create([
+        "name", "type", "hp", "attack", "defense", "speed"
     ], [
-        req.body.name, req.body.eaten
-    ], function (result) {
-        // Send back the ID of the new burger
+        req.body.name, req.body.type, req.body.hp, req.body.attack, req.body.defense, req.body.speed
+    ], function(result) {
         res.json({ id: result.insertId });
     });
 });
 
-router.put("/api/burger/:id", function (req, res) {
+// router.put("/api/generate/:id", function (req, res) {
+//     var condition = "id = " + req.params.id;
+
+//     console.log("condition", condition);
+
+//     generate.update({
+//         eaten: req.body.eaten
+//     }, condition, function(result) {
+//         if (result.changedRows == 0) {
+//             return res.status(404).end();
+//         } else {
+//             res.status(200).end();
+//         }
+//     });
+// });
+
+router.delete("/api/generate/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
-    console.log("condition", condition);
-    console.log(req.body.eaten)
-
-    burger.update({
-        eaten: req.body.eaten
-    }, condition, function (result) {
-        if (result.changedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-});
-
-router.delete("/api/burger/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-
-    burger.delete(condition, function (result) {
+    generate.delete(condition, function(result) {
         if (result.affectedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
             return res.status(404).end();
         } else {
             res.status(200).end();
@@ -57,5 +68,4 @@ router.delete("/api/burger/:id", function (req, res) {
     });
 });
 
-// Export routes for server.js to use.
 module.exports = router;
