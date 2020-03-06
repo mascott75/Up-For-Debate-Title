d3f5b50bd56f9ba7a4ba8812ad3f982154370498
Francisco Goya <degoyafran@gmail.com>
2020-03-06 17:30:41 -0600

    random pokemon image set

--- public/js/Generate/generate.js
+++ public/js/Generate/generate.js
@@ -1,18 +1,18 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // delete function
-  $(".delete-pokemon").on("click", function () {
+  $(".delete-pokemon").on("click", function() {
     var id = $(this).data("id");
     // Send the DELETE request.
     $.ajax("/api/generate/" + id, {
       type: "DELETE"
-    }).then(function () {
+    }).then(function() {
       console.log("deleted pokemon", id);
       // Reload the page to get the updated list
       location.reload();
     });
   });
   // post function
-  $(".create-pokemon").on("submit", function (event) {
+  $(".create-pokemon").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
     var newPokemon = {
@@ -29,7 +29,7 @@ $(document).ready(function () {
     $.ajax("/api/generate", {
       type: "POST",
       data: newPokemon
-    }).then(function () {
+    }).then(function() {
       console.log("created new Pokemon!!");
       // Reload the page to get the updated list
       location.reload();
@@ -58,29 +58,57 @@ function getRandomType() {
   }
   return type;
 }
-$("#find-pokemon").on("click", function(event) {
+
+$("#btn1Gen").on("click", function(event) {
+  console.log("is working");
   // event.preventDefault() can be used to prevent an event's default behavior.
   // Here, it prevents the submit button from trying to submit a form when clicked
   event.preventDefault();
   // Here we grab the text from the input box
-  var name = $("#imgPokeOne").val();
+  var one = Math.floor(Math.random() * (151 - 1 + 1) + 1); // x = 1, y = 151
+  Math.floor(Math.random() * 150 + 1);
+
+  var two = Math.floor(Math.random() * (151 - 1 + 1) + 1);
+  Math.floor(Math.random() * 150 + 1);
+
+  var attack = Math.floor(Math.random() * (3113 - 1 + 1) + 1);
+  Math.floor(Math.random() * 3112 + 1);
+
+
+  console.log(one);
+  console.log(two);
+
+  // var name = $("#imgPokeOne").val();
+
   // Here we construct our URL
-  var queryURL = "https://pokeapi.co/api/v2/pokemon/" + name;
+  var queryURL =
+    "https://images.alexonsager.net/pokemon/fused/" +
+    one +
+    "/" +
+    one +
+    "." +
+    two +
+    ".png";
+
+  $("#imgPokeGen").attr("src", queryURL);
+  $("#attGen").text(attack);
+  console.log(attack);
   // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
   // and display it in the div with an id of movie-view
   // ------YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE.
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    var imgPokeOne = response.data.sprite.front_default;
-    $("#imgPokeOne").attr("src", imgPokeOne);
-  });
-});
-// -----------------------------------------------------------------------
-$(document).ready(function() {
-  $("#btn1").on("click", function() {});
-  $("#start-btn").on("click", function() {
-    // starts the battle
-  });
+  //   $.ajax({
+  //     url: queryURL,
+  //     method: "GET"
+  //   }).then(function(response) {
+  //     var imgPokeOne = response.data.sprite.front_default;
+  //     $("#imgPokeOne").attr("src", imgPokeOne);
+  //     console.log(response);
+  //   });
+  // });
+  // // -----------------------------------------------------------------------
+  // $(document).ready(function() {
+  //   $("#btn1").on("click", function() {});
+  //   $("#start-btn").on("click", function() {
+  //     // starts the battle
+  //   });
 });
