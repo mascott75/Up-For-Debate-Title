8353b4326eea9aa1131123bc4347826dde1912eb
HanFue282 <59034654+HanFue282@users.noreply.github.com>
2020-03-08 18:46:56 -0500

    saving point for generate.js

--- public/js/Generate/generate.js
+++ public/js/Generate/generate.js
@@ -90,9 +90,49 @@ $("#find-pokemon").on("click", function(event) {
 });
 // -----------------------------------------------------------------------
 $(document).ready(function() {
-  $("#btn1").on("click", function() {});
+  $("#btn1Gen").on("click", function() {});
 
   $("#start-btn").on("click", function() {
     // starts the battle
   });
+});
+
+$("#btn1Gen").on("click", function(event) {
+  // event.preventDefault() can be used to prevent an event's default behavior.
+  // Here, it prevents the submit button from trying to submit a form when clicked
+  event.preventDefault();
+  // Here we append img url with random number between 1-151 to pull up random image
+  var one = Math.floor(Math.random() * (151 - 1 + 1) + 1);
+  Math.floor(Math.random() * 150 + 1);
+  var two = Math.floor(Math.random() * (151 - 1 + 1) + 1);
+  Math.floor(Math.random() * 150 + 1);
+  // Here we create a variable to generate random number for different traits
+  var hp = Math.floor(Math.random() * (255 - 1 + 1) + 1);
+  Math.floor(Math.random() * 254 + 1);
+  var attack = Math.floor(Math.random() * (526 - 5 + 1) + 5);
+  Math.floor(Math.random() * 525 + 1);
+  var defense = Math.floor(Math.random() * (250 - 5 + 1) + 5);
+  Math.floor(Math.random() * 249 + 1);
+  var speed = Math.floor(Math.random() * (260 - 5 + 1) + 5);
+  Math.floor(Math.random() * 259 + 1);
+  // img URL
+  var queryURL =
+    "https://images.alexonsager.net/pokemon/fused/" +
+    one +
+    "/" +
+    one +
+    "." +
+    two +
+    ".png";
+  $("#imgPokeGen").attr("src", queryURL);
+  // input trait numeric value
+  $("#hpGen").text("HP: " + hp);
+  $("#attGen").text("Attack: " + attack);
+  $("#defGen").text("Defense: " + defense);
+  $("#spGen").text("Speed: " + speed);
+});
+$("#btn3Gen").on("click", function(event) {
+  event.preventDefault();
+  var value = $("#pokeName").val();
+  $("select").append("<option>" + value + "</option>");
 });
