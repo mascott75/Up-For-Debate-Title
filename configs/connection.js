13c778b712ef9de2a82a73043dd623940e758e26
HanFue282 <59034654+HanFue282@users.noreply.github.com>
2020-03-08 15:53:56 -0500

    testing of fred's code

--- configs/connection.js
+++ configs/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "root",
-  database: "passport_demo"
+  database: "pokemon_db"
 });
 
 connection.connect(function(err) {
