697b89b09f53ffaec99a9d9fb9106588f79ad330
Mason Scott <scottm7575@gmail.com>
2020-03-09 10:57:32 -0500

    finished generate page

--- public/js/Generate/generate.js
+++ public/js/Generate/generate.js
@@ -20,7 +20,6 @@ function getRandomType() {
   }
   return type;
 }
-
 $("#btn1Gen").on("click", function(event) {
   type = getRandomType();
   // event.preventDefault() can be used to prevent an event's default behavior.
@@ -29,24 +28,17 @@ $("#btn1Gen").on("click", function(event) {
   // Here we append img url with random number between 1-151 to pull up random image
   var one = Math.floor(Math.random() * (151 - 1 + 1) + 1);
   Math.floor(Math.random() * 150 + 1);
-
   var two = Math.floor(Math.random() * (151 - 1 + 1) + 1);
   Math.floor(Math.random() * 150 + 1);
-
   // Here we create a variable to generate random number for different traits
-
   hp = Math.floor(Math.random() * (255 - 1 + 1) + 1);
   Math.floor(Math.random() * 254 + 1);
-
   attack = Math.floor(Math.random() * (526 - 5 + 1) + 5);
   Math.floor(Math.random() * 525 + 1);
-
   defense = Math.floor(Math.random() * (250 - 5 + 1) + 5);
   Math.floor(Math.random() * 249 + 1);
-
   speed = Math.floor(Math.random() * (260 - 5 + 1) + 5);
   Math.floor(Math.random() * 259 + 1);
-
   // img URL
   var queryURL =
     "https://images.alexonsager.net/pokemon/fused/" +
@@ -56,33 +48,46 @@ $("#btn1Gen").on("click", function(event) {
     "." +
     two +
     ".png";
-
   $("#imgPokeGen").attr("src", queryURL);
-
   // input trait numeric value
-
   $("#hpGen").text("HP: " + hp);
   $("#typGen").text("Type: " + type);
   $("#attGen").text("Attack: " + attack);
   $("#defGen").text("Defense: " + defense);
   $("#spGen").text("Speed: " + speed);
 });
-
 $("#btn3Gen").on("click", function(event) {
   event.preventDefault();
-
-  var value = $("#pokeName").val();
-
-  $("select").append("<option>" + value + "</option>");
-
-  var stats = {
-    HP: hp,
-    Type: type,
-    Attack: attack,
-    Defense: defense,
-    Speed: speed
-  };
+  var value = $("#pokeName")
+    .val()
+    .trim();
+  // if (value  >3){  document.getElementById("btn3Gen").disabled = false};
+  if (value !== "") {
+    $("select").append(`<option value ="${value}">${value}</option>`);
+    var stats = {
+      name: value,
+      type: type,
+      hp: hp,
+      attack: attack,
+      defense: defense,
+      speed: speed
+    };
+  } else {
+    alert("type name");
+  }
   console.log(stats);
-
   $.post("/api/posts/", stats);
+});
+var selectOne = document.getElementById("selectOne");
+selectOne.addEventListener("change", function(event) {
+  console.log(event.target.value);
+  $.get(`/api/chosen/${event.target.value}`).then(function(data) {
+    $("#hpGen").text("HP: " + data.hp);
+    $("#typGen").text("Type: " + data.type);
+    $("#attGen").text("Attack: " + data.attack);
+    $("#defGen").text("Defense: " + data.defense);
+    $("#spGen").text("Speed: " + data.speed);
+    // document.getElementById("btn3Gen").disabled = false;
+    // console.log(data.attack)
+  });
 });
