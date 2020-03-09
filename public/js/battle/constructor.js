4a45265a721c2dcfac98a0a6825b244197f47b35
Mason Scott <scottm7575@gmail.com>
2020-03-09 09:49:30 -0500

    finished the battle page. for now...

--- public/js/battle/constructor.js
+++ public/js/battle/constructor.js
@@ -1,92 +1,86 @@
-var textarea = $("#textarea")
+export class Character {
+  constructor(name, type, hp, attack, defense, speed) {
+    this.name = name;
+    this.type = type;
+    this.hp = hp;
+    this.atk = attack;
+    this.defense = defense;
+    this.speed = speed;
+    this.hasBerry = true;
+  }
 
-class Character {
-    constructor(name, type, hp, attack, defense, speed) {
-      this.name = name;
-      this.type = type;
-      this.hp = hp;
-      this.attack = attack;
-      this.defense = defense;
-      this.speed = speed;
+  // method which determines whether or not a character's "hitpoints" are less then zero
+  // and returns true or false depending upon the outcome
+  isAlive() {
+    if (this.hp <= 0) {
+      console.log(`${this.name} has been defeated!`);
+      return false;
     }
-  
-    // method which determines whether or not a character's "hitpoints" are less then zero
-    // and returns true or false depending upon the outcome
-    isAlive() {
-      if (this.hp <= 0) {
-        console.log(`${this.name} has been defeated!`);
-        return false;
+    return true;
+  }
+
+  // method which takes in a second object and decreases their "hitPoints" by this character's strength
+  attack(opponent) {
+    var critChance = Math.floor(Math.random() * 10);
+
+    var attackValue = (this.atk * 20) / opponent.defense;
+
+    if (critChance !== 0) {
+      attackValue = (this.atk * 30) / opponent.defense;
+      console.log(attackValue);
+
+      switch (this.type) {
+        case "Water":
+          if (opponent.type === "Fire") {
+            opponent.hp -= attackValue * 2;
+            return `${this.name} hit ${opponent.name} for ${attackValue}, It's super effective! \n`;
+          } else if (opponent.type === "Dragon") {
+            opponent.hp -= attackValue * 0.5;
+            return `${this.name} hit ${opponent.name} for ${attackValue *
+              0.5}, It's not very effective \n`;
+          } else {
+            opponent.hp -= attackValue;
+            return `${this.name} hit ${opponent.name} for ${attackValue} \n`;
+          }
+        case "Fire":
+          if (opponent.type === "Grass") {
+            opponent.hp -= attackValue * 2;
+            return `${this.name} hit ${opponent.name} for ${attackValue}, It's super effective! \n`;
+          } else if (opponent.type === "Dragon") {
+            opponent.hp -= attackValue * 0.5;
+            return `${this.name} hit ${opponent.name} for ${attackValue *
+              0.5}, It's not very effective \n`;
+          } else {
+            opponent.hp -= attackValue;
+            return `${this.name} hit ${opponent.name} for ${attackValue} \n`;
+          }
+
+        case "Grass":
+          if (opponent.type === "Water") {
+            opponent.hp -= attackValue * 2;
+            return `${this.name} hit ${opponent.name} for ${attackValue}, It's super effective! \n`;
+          } else if (opponent.type === "Dragon") {
+            opponent.hp -= attackValue * 0.5;
+            return `${this.name} hit ${opponent.name} for ${attackValue *
+              0.5}, It's not very effective \n`;
+          } else {
+            opponent.hp -= attackValue;
+            return `${this.name} hit ${opponent.name} for ${attackValue} \n`;
+          }
+
+        case "Dragon":
+          opponent.hp -= attackValue;
+          return `${this.name} hit ${opponent.name} for ${attackValue} \n`;
       }
-      return true;
+    } else if (critChance === 0) {
+      return `${this.name} attacked! \n ${opponent.name} avoided the attack! \n`;
     }
-  
-    // method which takes in a second object and decreases their "hitPoints" by this character's strength
-    attack(opponent) {
-      var critChance = Math.floor(Math.random()*10)
-      
-      var attackValue = ((this.attack * 20) / opponent.defense)
-      
-      if (critChance !== 0){
-        attackValue = ((this.attack * 30) / opponent.defense)
+  }
 
-        switch (this.type) {
-            case water:
-                if(opponent.type === fire){
-                    textArea.textContent += (`${this.name} hit ${opponent.name} for ${attackValue}, It's super effective! \n`);
-                    opponent.hp -= (attackValue * 2);
-                }
-                else if (opponent.type === dragon){
-                    textArea.textContent += (`${this.name} hit ${opponent.name} for ${attackValue * .5}, It's not very effective \n`);
-                    opponent.hp -= (attackValue * .5);
-                }   
-                else{
-                    textArea.textContent += (`${this.name} hit ${opponent.name} for ${attackValue} \n`);
-                    opponent.hp -= (attackValue);
-                }
-            break;
-            case fire:
-                if(opponent.type === grass){
-                    textArea.textContent += (`${this.name} hit ${opponent.name} for ${attackValue}, It's super effective! \n`);
-                    opponent.hp -= (attackValue * 2);
-                }
-                else if (opponent.type === dragon){
-                    textArea.textContent += (`${this.name} hit ${opponent.name} for ${attackValue * .5}, It's not very effective \n`);
-                    opponent.hp -= (attackValue * .5);
-                }   
-                else{
-                    textArea.textContent += (`${this.name} hit ${opponent.name} for ${attackValue} \n`);
-                    opponent.hp -= (attackValue);
-                }
-            break;
-            case grass:
-                if(opponent.type === water){
-                   textArea.textContent += (`${this.name} hit ${opponent.name} for ${attackValue}, It's super effective! \n`);
-                    opponent.hp -= (attackValue * 2);
-                }
-                else if (opponent.type === dragon){
-                   textArea.textContent += (`${this.name} hit ${opponent.name} for ${attackValue * .5}, It's not very effective \n`);
-                    opponent.hp -= (attackValue * .5);
-                }   
-                else{
-                   textArea.textContent += (`${this.name} hit ${opponent.name} for ${attackValue} \n`);
-                    opponent.hp -= (attackValue);
-                }
-            break;
-            case dragon:     
-                   textArea.textContent += (`${this.name} hit ${opponent.name} for ${attackValue} \n`);
-                    opponent.hp -= (attackValue);
-               
-                }
-      }     
-               else if(critChance === 0){
-                    textArea.textContent += `${this.name} attacked! \n ${opponent.name} avoided the attack! \n` 
-                  }
-     
+  eatBerry() {
+    if (this.hp <= 30 && this.hasBerry) {
+      this.hp += 30;
+      this.hasBerry = false;
     }
-
-    // eatBerry{
-
-    // }
+  }
 }
-
-// module.exports(Character)
--- public/js/battle/pokemon.js
+++ public/js/battle/pokemon.js
@@ -1,3 +1,48 @@
+var pokemon1;
+var pokemon2;
+var textArea = $("#textarea");
+
+import { Character } from "./constructor.js";
+
+$("#btn1").on("click", function() {
+  battle(pokemon1, pokemon2);
+});
+function battle(pokemon1, pokemon2) {
+  var battleString = "";
+  while (pokemon1.isAlive() && pokemon2.isAlive()) {
+    if (pokemon1.speed > pokemon2.speed) {
+      pokemon1.eatBerry();
+      pokemon2.eatBerry();
+      battleString += pokemon1.attack(pokemon2);
+      battleString += pokemon2.attack(pokemon1);
+    } else {
+      pokemon2.eatBerry();
+      pokemon1.eatBerry();
+      battleString += pokemon2.attack(pokemon1);
+      battleString += pokemon1.attack(pokemon2);
+    }
+    updateField1();
+    updateField2();
+  }
+  if (pokemon1.isAlive()) {
+    battleString += `${pokemon2.name} is defeated!!`;
+  } else {
+    battleString += `${pokemon1.name} is defeated!!`;
+  }
+  textArea.text(battleString);
+}
+function updateField1() {
+  $("#liHpOne").text("HP: " + pokemon1.hp);
+  $("#liAttOne").text("Atk: " + pokemon1.atk);
+  $("#liDefOne").text("Def: " + pokemon1.defense);
+  $("#liSpOne").text("Spd: " + pokemon1.speed);
+}
+function updateField2() {
+  $("#liHpTwo").text("HP: " + pokemon2.hp);
+  $("#liAttTwo").text("Atk: " + pokemon2.atk);
+  $("#liDefTwo").text("Def: " + pokemon2.defense);
+  $("#liSpTwo").text("Spd: " + pokemon2.speed);
+}
 $.get("/generate", function(generated) {
   $.get("/api/pokemon", function(pokemon) {
     pulDownPop(pokemon, generated);
@@ -5,13 +50,12 @@ $.get("/generate", function(generated) {
 });
 
 function pulDownPop(pokemon, generated) {
-  
   var nameTag = "<option value='0'>Select</option>";
 
-  for (j = 0; j < generated.length; j++) {
+  for (var j = 0; j < generated.length; j++) {
     nameTag += "<option value='P'>" + generated[j].name + "</option>";
   }
-  for (i = 0; i < pokemon.length; i++) {
+  for (var i = 0; i < pokemon.length; i++) {
     nameTag += "<option value='U'>" + pokemon[i].name + "</option>";
   }
   $("#selectTwo").append($(nameTag));
@@ -19,13 +63,43 @@ function pulDownPop(pokemon, generated) {
   // document.getElementById("selectGen").innerHTML = nameTag;
 }
 
-$("#selectOne").on("change",populatePokemonSide("One"));
-$("#selectTwo").on("change",populatePokemonSide("Two"));
+$("#selectOne").on("change", populatePokemonSide1);
+$("#selectTwo").on("change", populatePokemonSide2);
 
-function populatePokemonSide(pullID,event) {
-
+function populatePokemonSide1() {
   console.log(this);
-  console.log(pullID);
+
+  // CCflag = true;
+  var pokemonName = $(`#${this.id} :selected`).text();
+  var pokemonTable = $(`#${this.id} :selected`).val();
+  var pokemonNameLength = pokemonName.length;
+  if (pokemonTable === "U") {
+    /*place call to populate*/
+    console.log("is a P");
+    console.log("name!!! " + pokemonName);
+    console.log("im selectVal2= " + pokemonTable);
+    console.log("im selectVal=" + pokemonNameLength);
+    $.get("/api/pokemon/" + pokemonName, function(res) {
+      console.log("is a P= " + JSON.stringify(res));
+      applyStats1(res);
+    });
+  } else {
+    /*place call to populate*/
+    console.log("is a U");
+    console.log("NAME " + pokemonName);
+    console.log("im selectVal2=" + pokemonTable);
+    console.log("im selectVal=" + pokemonNameLength);
+    $.get("/api/generated/" + pokemonName, function(res) {
+      console.log("is a U = " + JSON.stringify(res));
+    }).then(function(res) {
+      console.log("this is some " + JSON.stringify(res));
+      applyStats1(res);
+    });
+  }
+}
+function populatePokemonSide2() {
+  console.log(this);
+
   // CCflag = true;
   var pokemonName = $(`#${this.id} :selected`).text();
   var pokemonTable = $(`#${this.id} :selected`).val();
@@ -36,38 +110,102 @@ function populatePokemonSide(pullID,event) {
     console.log("name!!! " + pokemonName);
     console.log("im selectVal2=" + pokemonTable);
     console.log("im selectVal=" + pokemonNameLength);
-    $.get("/api/generated/" + pokemonName, function(res){
-      console.log("is a P="+res)
-      
+    $.get("/api/pokemon/" + pokemonName, function(res) {
+      console.log("is a P= " + JSON.stringify(res));
+      applyStats2(res, "#imgPokeTwo");
     });
   } else {
     /*place call to populate*/
     console.log("is a U");
-    console.log("NAME" + pokemonName);
+    console.log("NAME " + pokemonName);
     console.log("im selectVal2=" + pokemonTable);
     console.log("im selectVal=" + pokemonNameLength);
-    $.get("/api/pokemon/" + pokemonName, function(res){
-      console.log("is a U ="+res)
-      let displayData = res;
+    $.get("/api/generated/" + pokemonName, function(res) {
+      console.log("is a U = " + JSON.stringify(res));
+    }).then(function(res) {
+      console.log("this is some " + JSON.stringify(res));
+      applyStats2(res, "#imgPokeTwo");
     });
   }
-  let displayData = res;
-  pokemonDisplay(pullID,displayData);
 }
 
-function pokemonDisplay(pokemanStatDisplay,displayData) {
-  console.log("im in ID ="+pokemanStatDisplay);
-  console.log("display data ="+ displayData);
-  
-  // pokemanStatDisplay = "Two";
-  document.getElementById("pokeName" + pokemanStatDisplay).textContent =
-    "new text";
-  document.getElementById("imgPoke" + pokemanStatDisplay).src =
-    "http://placehold.it/100x100";
-  document.getElementById("liHp" + pokemanStatDisplay).textContent = "new text";
-  document.getElementById("liAtt" + pokemanStatDisplay).textContent =
-    "new text";
-  document.getElementById("liDef" + pokemanStatDisplay).textContent =
-    "new text";
-  document.getElementById("liSp" + pokemanStatDisplay).textContent = "new text";
+function applyStats1(res) {
+  $("#pokeNameOne").text(res.name);
+  $("#liHpOne").text("HP: " + res.hp);
+  $("#liAttOne").text("Atk: " + res.attack);
+  $("#liDefOne").text("Def: " + res.defense);
+  $("#liSpOne").text("Spd: " + res.speed);
+  pokeApiCall1(res.name);
+  pokemon1 = new Character(
+    res.name,
+    res.type,
+    res.hp,
+    res.attack,
+    res.defense,
+    res.speed
+  );
+}
+
+function applyStats2(res) {
+  $("#pokeNameTwo").text(res.name);
+  $("#liHpTwo").text("HP: " + res.hp);
+  $("#liAttTwo").text("Atk: " + res.attack);
+  $("#liDefTwo").text("Def: " + res.defense);
+  $("#liSpTwo").text("Spd: " + res.speed);
+  pokeApiCall2(res.name);
+  pokemon2 = new Character(
+    res.name,
+    res.type,
+    res.hp,
+    res.attack,
+    res.defense,
+    res.speed
+  );
+}
+
+function pokeApiCall1(resName) {
+  // Here we grab the text from the input box
+  var name = resName.toLowerCase();
+
+  // Here we construct our URL
+  var queryURL = "https://pokeapi.co/api/v2/pokemon/" + name;
+  console.log(queryURL);
+
+  // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
+  // and display it in the div with an id of movie-view
+
+  // ------YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE.
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    var imgPokeOne = response.sprites.front_default;
+    $("#imgPokeOne").attr("src", imgPokeOne);
+    console.log(imgPokeOne);
+  });
+}
+function pokeApiCall2(resName) {
+  // Here we grab the text from the input box
+  var name = resName.toLowerCase();
+
+  // Here we construct our URL
+  var queryURL = "https://pokeapi.co/api/v2/pokemon/" + name;
+  console.log(queryURL);
+
+  // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
+  // and display it in the div with an id of movie-view
+
+  // ------YOUR CODE GOES IN THESE DASHES. DO NOT MANUALLY EDIT THE HTML ABOVE.
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    var imgPokeOne = response.sprites.front_default;
+    $("#imgPokeTwo").attr("src", imgPokeOne);
+    console.log(imgPokeOne);
+  });
 }
